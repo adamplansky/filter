9e02dfde0470f16cb0a9f35182f46f0e789d20f6
Adam Plansky <adamplansky@gmail.com>
2016-11-10 15:19:58 +0100

    static prices json/yaml

---
+++ config/load_static_prices.py
@@ -0,0 +1,27 @@
+import logging
+import json
+import os
+import glob
+import shutil
+import threading
+import random
+import heapq
+from pprint import pprint
+
+CFG = 'static_prices.json'
+
+
+def load_cfg():
+    filename = CFG
+    print(filename)
+    with open(filename) as data_file:
+        data = json.load(data_file)
+        for val in data:
+            for k,v in val.items():
+                for kk,vv in v.items():
+                    _category = "{}.{}".format(k,kk)
+                    _price = vv
+                    print(_category, _price)
+
+
+load_cfg()
