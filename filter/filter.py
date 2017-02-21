035e624bed7b3520e8288284db1853b88ac5624b
Adam Plansky <adamplansky@gmail.com>
2017-02-21 12:02:03 +0100

    update to RabbitMQ

--- filter/filter.py
+++ filter/filter.py
@@ -1,18 +1,25 @@
 #!/usr/bin/env python3
 #python filter.py -i "u:hoststats-alerts,u:haddrscan-alerts"
+
 import logging
 import json
 import sys
 import os
+import pika
+import ssl
+
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from os.path import dirname as dirn
 import glob
+import pytz
 import shutil
 import threading
 import ipaddress
 import time
 from filter.mapping import Mapping
 import heapq
-from datetime import datetime
+from datetime import datetime, timedelta, timezone
 from math import log
 
 first = lambda h: 2**h - 1      # H stands for level height
@@ -32,10 +39,20 @@ def hprint(heap, width=None):
         below = 2 ** (height - h - 1)
         field = (2 * below - 1) * width
         print(gap.join(prepare(e, field) for e in level(heap, h)))
-logging.basicConfig(
-    level=logging.DEBUG,
-    format='(%(threadName)-10s) %(message)s',
-)
+# logging.basicConfig(
+#     level=logging.DEBUG,
+#     format='(%(threadName)-10s) %(message)s',
+# )
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 class FolderDispatcher(threading.Thread):
     # logging.basicConfig(filename="example.log",level=logging.DEBUG)
     ROOT_PATH = os.path.realpath(dirn(dirn(os.path.abspath(__file__))))
@@ -97,13 +114,58 @@ class FolderDispatcher(threading.Thread):
                         self.move_to_error_folder(filename)
                 self.shared_thread_event.set()
             time.sleep(1)
+class RabbitMqDispatcher(threading.Thread):
+    def __init__(self, shared_array, event):
+        threading.Thread.__init__(self)
+        self.shared_array = shared_array
+        self.shared_thread_event = event
+
+    def run(self):
+        logging.debug('running RabbitMqDispatcher')
+        self.idea_dispatcher()
+
+    def idea_dispatcher(self):
+        ssl_options = {
+            "ca_certs":"/Users/adamplansky/Desktop/message_app/testca/cacert.pem",
+            "certfile": "/Users/adamplansky/Desktop/message_app/client/cert.pem",
+            "keyfile": "/Users/adamplansky/Desktop/message_app/client/key.pem",
+            "cert_reqs": ssl.CERT_REQUIRED,
+            "ssl_version":ssl.PROTOCOL_TLSv1_2
+        }
+        credentials = pika.PlainCredentials(os.environ['RABBITMQ_USERNAME'], os.environ['RABBITMQ_PASSWORD'])
+        parameters = pika.ConnectionParameters(host='192.168.2.120', port=5671, virtual_host='/', heartbeat_interval = 0, credentials=credentials, ssl = True, ssl_options = ssl_options)
+        connection = pika.BlockingConnection(parameters)
+        channel = connection.channel()
+
+        # ''' start subscriber '''
+
+        channel.exchange_declare(exchange='broadcast_idea', exchange_type='fanout') #fanout == type broadcast #exchange == name
+        result = channel.queue_declare(exclusive=True) #i want random name of queue, exlusive == true, when we disconnect delete queue
+        queue_name = result.method.queue
+        channel.queue_bind(exchange='broadcast_idea', queue=queue_name)
+        print(' [*] Waiting for ideas. To exit press CTRL+C')
+        m = Mapping()
+        def callback(ch, method, properties, body):
+            data = json.loads(body.decode("utf-8"))
+            idea_alert = m.map_alert_to_hash(data)
+            da_alert = AlertExtractor.parse_alert(idea_alert)
+            if da_alert is not None:
+                self.shared_array.append( da_alert )
+                self.shared_thread_event.set()
+        channel.basic_consume(callback, queue=queue_name, no_ack=True)
+        channel.start_consuming()
 
 
 class AlertExtractor:
+    @classmethod
+    def extract_ip_and_direction(cls, dir_and_ip):
+        return dir_and_ip[:1], dir_and_ip[1:]
 
     @classmethod
     def parse_datetime(cls, datetime_string):
-        return datetime.strptime(datetime_string, '%Y-%m-%dT%H:%M:%SZ')
+        #todo: not nice
+        local_tz = pytz.timezone ("UTC")
+        return local_tz.localize(datetime.strptime(datetime_string, '%Y-%m-%dT%H:%M:%SZ'))
 
     @classmethod
     def get_detect_time(cls, alert):
@@ -171,7 +233,7 @@ class AlertDatabase:
         self.database = {}
 
     def get_ip_prefix(self, ips):
-        print("get_ip_prefix", ips)
+        #print("get_ip_prefix", ips)
         if(len(ips) == 0 ): return None,[]
         if(len(ips[0]) > 0): return "S", ips[0]
         if(len(ips[1]) > 0): return "T", ips[1]
@@ -182,36 +244,63 @@ class AlertDatabase:
         #todo: check if it works
         #probably some test is needed
         return max([x[1] for x in self.database[ip]])
+    def get_last_score(self,ip):
+        if(len(self.database[ip]["alerts"]) > 0):
+            return self.database[ip]["alerts"][-1][1]
+        return -1
 
     def print_database(self):
         print("-----DATABASE-----")
-        sum_len = 0
         for key, value in self.database.items() :
-            sum_len += len(value)
-            #print (key, len(value))
-        database_len = len(self.database)
-        print(database_len, " / ", sum_len)
+            print ("{} -> {}/{}".format(key, value["cnt_hour"],value["cnt"]))
         print("-----DATABASE-----")
+
+    def get_cnt_hour(self, ip):
+        return self.database[ip]["cnt_hour"]
+
+    def get_cnt(self, ip):
+        return self.database[ip]["cnt"]
+
+    def recalculate_cnt_hour(self, ip):
+        date_min = datetime.now(timezone.utc) - timedelta(hours=1)
+        cnt_hour = 0
+        for idx, da_alert in enumerate(self.database[ip]["alerts"]):
+            if(date_min < da_alert[0]):
+                cnt_hour+=1
+                #print(da_alert[0])
+            else:
+                del self.database[ip]["alerts"][idx]
+        self.database[ip]["cnt_hour"] = cnt_hour
+        #print(date_min < self.database[ip]["alerts"][0])
+        #self.database[ip]["cnt_hour"] = cnt_hour
+        # print("##############################")
+        # print(self.database[ip])
+        # print("##############################")
+
 
     def database(self):
         return self.database
 
     def add(self,da_alert):
+        #print("da_alert: ", da_alert)
         if da_alert is None: return
         prefix, ips = self.get_ip_prefix(da_alert["ips"])
-        print("add: ",prefix, ips)
+        #print("add: ",prefix, ips)
         ips_to_return = []
         for ip_ary in ips:
             ip = prefix + ip_ary
-            if not ip in self.database: self.database[ip] = []
+            if not ip in self.database: self.database[ip] = {"cnt": 0, "alerts": [], "cnt_hour": 0}
             ips_to_return.append(ip)
-            self.database[ip].append([
+            self.database[ip]["alerts"].append([
                                       da_alert["time"],
                                       da_alert["score"],
                                       da_alert["node"]
                                      ])
+            self.database[ip]["cnt"] += 1
+            self.recalculate_cnt_hour(ip)
 
-        self.print_database()
+
+        #sself.print_database()
         return ips_to_return
 
 class HeapOutput:
@@ -333,11 +422,15 @@ class Filter(threading.Thread):
         self.heap_output = HeapOutput()
         self.argv_param = argv_param
         self.alert_database = AlertDatabase()
+        self.global_filter_cnt = 0
+        self.global_capture_filter_cnt = 0
 
     def run(self):
         logging.debug('running Filter')
         if self.argv_param == '-f':
             fd = FolderDispatcher(self.shared_array,self.shared_thread_event)
+        elif self.argv_param == '-RMQ':
+            fd = RabbitMqDispatcher(self.shared_array,self.shared_thread_event)
         #elif self.argv_param == '-i':
             #fd = UnixSocketDispatcher(self,self.shared_thread_event)
 
@@ -361,12 +454,32 @@ class Filter(threading.Thread):
                 ips = self.alert_database.add(idea_alert)
 
                 for ip in ips:
-                    score = self.alert_database.get_max_score(ip)
-                    print("get score for ip {} {} ".format(ip,score))
-                    print("alert: ", self.alert_database.database[ip])
-                print("-------")
+                    #todo: cnt_hour pouze pokud add += 1 jinak nic
+                    score = self.alert_database.get_last_score(ip)
+                    #cnt_hour = self.alert_database.get_cnt_hour(ip)
+                    cnt = self.alert_database.get_cnt(ip)
+                    #print("get score for ip {} score: {} cnt_hour: {}".format(ip,score, cnt_hour))
+                    #print("alert: ", self.alert_database.database[ip])
+                    self.global_filter_cnt += 1
+                    if(score > 1 or (score == 1 and (cnt == 5 or cnt % 100 == 0))):
+                        #posli pozadavek o zachyt
+                        CaptureRequest.send(ip)
+                        self.global_capture_filter_cnt += 1
+                        print(bcolors.WARNING +  "{}/{}".format(self.global_capture_filter_cnt,self.global_filter_cnt ) + bcolors.ENDC)
+                    #else:
+                        #print("Capture requirments: " +bcolors.WARNING +  "Not satisfied" + bcolors.ENDC)
 
 
+
+
+                #print("-------")
+
+
+class CaptureRequest():
+    @classmethod
+    def send(cls, dir_and_ip):
+        direction, ip = AlertExtractor.extract_ip_and_direction(dir_and_ip)
+        print(bcolors.OKGREEN + "Capture Request => ip: {}, direction: {}. packets: {}, seconds: {}".format(ip, direction, 10000, 300) + bcolors.ENDC)
 
 class FilterMain():
     def __init__(self, argv_param):
@@ -379,11 +492,14 @@ def help():
     Options:
           -i "u:socket1,u:socket2" - to use filter with NEMEA unixsockets. Number of sockets is variable."
           -f - filter reads IDEA jsons from /jsons folder
+          -RMQ - rabbitmq - hardcoded parameters
           """
 if __name__ == '__main__':
     print(sys.argv)
     if(len(sys.argv) == 1 or sys.argv[1] in ["-h","--help"]):
         print(help())
+    elif(sys.argv[1] == "-RMQ"):
+        FilterMain(sys.argv[1])
     elif(sys.argv[1] == "-f"):
         FilterMain(sys.argv[1])
     elif(sys.argv[1] == "-i"):
--- test/filter_test.py
+++ test/filter_test.py
@@ -3,21 +3,61 @@ import unittest
 import os
 import sys
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from filter.filter import AlertDatabase
-import datetime
+from filter.filter import AlertDatabase, AlertExtractor
+#import datetime
+from datetime import datetime, timedelta, timezone
 
 class MyTest(unittest.TestCase):
     def test_get_max_score(self):
         ad = AlertDatabase()
-        a = [[datetime.datetime(2017, 2, 14, 6, 49, 33), 4, ['cz.cesnet.au1.warden_filer', 'cz.cesnet.labrea']], [datetime.datetime(2017, 2, 14, 6, 49, 33), 3, ['cz.cesnet.au1.warden_filer', 'cz.cesnet.labrea']]]
+        a = [[datetime(2017, 2, 14, 6, 49, 33), 4, ['cz.cesnet.au1.warden_filer', 'cz.cesnet.labrea']], [datetime(2017, 2, 14, 6, 49, 33), 3, ['cz.cesnet.au1.warden_filer', 'cz.cesnet.labrea']]]
         ad.database["S1.2.3.4"] = a
         self.assertEqual(ad.get_max_score("S1.2.3.4"), 4)
 
-    def test_get_max_score_1(self):
+    def test_get_last_score(self):
         ad = AlertDatabase()
-        b = [[datetime.datetime(2017, 2, 14, 6, 49, 33), 10, ['cz.cesnet.au1.warden_filer', 'cz.cesnet.labrea']], [datetime.datetime(2017, 2, 14, 6, 49, 33), 3, ['cz.cesnet.au1.warden_filer', 'cz.cesnet.labrea']]]
-        ad.database["S1.2.3.5"] = b
-        self.assertEqual(ad.get_max_score("S1.2.3.5"), 10)
+        da_alert = {'node': ['cz.cesnet.au1.warden_filer', 'cz.cesnet.labrea'], 'score': 1, 'time': datetime(2017, 2, 13, 19, 23, 50), 'ips': [['88.250.248.71'], ['78.128.254.197']]}
+        da_alert1 = {'node': ['cz.cesnet.au1.warden_filer', 'cz.cesnet.labrea'], 'score': 2, 'time': datetime.now(timezone.utc), 'ips': [['88.250.248.71'], ['78.128.254.197']]}
+        da_alert2 = {'node': ['cz.cesnet.au1.warden_filer', 'cz.cesnet.labrea'], 'score': 3, 'time': datetime.now(timezone.utc), 'ips': [['88.250.248.71'], ['78.128.254.197']]}
+        ad.add(da_alert)
+        ad.add(da_alert1)
+        ad.add(da_alert2)
+
+        self.assertEqual(ad.get_last_score('S88.250.248.71'), 3)
+    def test_get_last_score(self):
+        ad = AlertDatabase()
+        da_alert = {'node': ['cz.cesnet.au1.warden_filer', 'cz.cesnet.labrea'], 'score': 1, 'time': datetime.now(timezone.utc) - timedelta(days=1) , 'ips': [['88.250.248.71'], ['78.128.254.197']]}
+        ad.add(da_alert)
+
+        self.assertEqual(ad.get_last_score('S88.250.248.71'), -1)
+
+    def test_test_last_score(self):
+        pass
+    def test_extract_ip_and_direction(self):
+        direction, ip = AlertExtractor.extract_ip_and_direction("S1.2.3.4")
+        self.assertEqual(direction, "S")
+        self.assertEqual(ip, "1.2.3.4")
+
+    # def test_recalculate_cnt_hour(self):
+    #     print("yOyO")
+    #     da_alert = {'node': ['cz.cesnet.au1.warden_filer', 'cz.cesnet.labrea'], 'score': 1, 'time': datetime.datetime(2017, 2, 13, 19, 23, 50), 'ips': [['88.250.248.71'], ['78.128.254.197']]}
+    #     da_alert1 = {'node': ['cz.cesnet.au1.warden_filer', 'cz.cesnet.labrea'], 'score': 1, 'time': datetime.datetime.now(), 'ips': [['88.250.248.71'], ['78.128.254.197']]}
+    #     da_alert2 = {'node': ['cz.cesnet.au1.warden_filer', 'cz.cesnet.labrea'], 'score': 1, 'time': datetime.datetime.now(), 'ips': [['88.250.248.72'], ['78.128.254.195']]}
+    #     da_alert3 = {'node': ['cz.cesnet.au1.warden_filer', 'cz.cesnet.labrea'], 'score': 1, 'time': datetime.datetime.now() - datetime.timedelta(minutes=1), 'ips': [['88.250.248.72'], ['78.128.254.195']]}
+    #     da_alert4 = {'node': ['cz.cesnet.au1.warden_filer', 'cz.cesnet.labrea'], 'score': 1, 'time': datetime.datetime.now() - datetime.timedelta(minutes=5), 'ips': [['88.250.248.72'], ['78.128.254.195']]}
+    #     da_alert5 = {'node': ['cz.cesnet.au1.warden_filer', 'cz.cesnet.labrea'], 'score': 1, 'time': datetime.datetime.now() - datetime.timedelta(minutes=100), 'ips': [['88.250.248.72'], ['78.128.254.195']]}
+    #     ad = AlertDatabase()
+    #     ad.add(da_alert)
+    #     ad.add(da_alert1)
+    #     ad.add(da_alert2)
+    #     ad.add(da_alert3)
+    #     ad.add(da_alert4)
+    #     ad.add(da_alert5)
+    #     print(ad.database())
+    #     #print("XOXO")
+    #     #print(ad.database()["S88.250.248.71"]["alerts"])
+    #     ad.add(da_alert2)
+
 
 if __name__ == '__main__':
     unittest.main()
